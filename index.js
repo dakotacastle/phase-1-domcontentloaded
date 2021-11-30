430f79acbd53d181537f9c54013c066dcb167d87
Dakota Castle <barrettcastle@gmail.com>
2021-11-30 13:42:35 -0500

    commit tree c2bb85ac44b0c463fede94d7ab6ec87eb8f6042b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// function changeText() {
+//     const element = document.getElementById('text')
+//     element.textContent = 'This is really cool!'
+// }
+// document.addEventListener('DOMContentLoaded', changeText)
+
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.getElementById('text')
+    element.textContent = 'This is really cool!'
+    console.log("The DOM has loaded");
+  });
